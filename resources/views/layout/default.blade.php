b4ff3f8a8215290e7611b0e56d8408a9ebf5890a
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-11-15 20:48:14 +0100

    Add bootstrap to project

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -5,8 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    {{-- Libraries --}}
+    @vite(['node_modelules/bootstrap/js/index.esm.js', 'node_modules/bootstrap/scss/bootstrap.scss'])
 </head>
 <body>
-    @yield('content')
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
