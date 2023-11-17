803b9dbe834a71415ccb042bfeb46bfb5aad5100
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-11-17 01:28:38 +0100

    Create footer

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,10 +10,13 @@
     {{-- Libraries --}}
     @vite(['node_modelules/bootstrap/js/index.esm.js', 'node_modules/bootstrap/scss/bootstrap.scss'])
 </head>
+
 <body>
     @include('layout.navbar')
     <div class="container">
         @yield('content')
+        @include('layout.footer')
     </div>
 </body>
+
 </html>
